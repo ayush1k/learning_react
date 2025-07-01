438435491dc7fd6a03404a4bd539ca0e32d58b85
Ayush Kumar <ayushkumar47834@gmail.com>
2025-07-01 20:03:03 +0000

    half lec 14 done

--- 10-todo-context-local/src/App.jsx
+++ 10-todo-context-local/src/App.jsx
@@ -4,7 +4,23 @@ import './App.css'
 
 function App() {
 
-  const [todo, setTodo] =  useState([])
+  const [todos, setTodos] =  useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id:Date.now(), ...todo}, ...prev])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  const deleteTodo = () => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = () => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} :prevTodo))
+  }
 
   return (
     <TodoProvider value={{todos, addtodo, updateTodo, deleteTodo, toggleComplete}}>
@@ -12,7 +28,7 @@ function App() {
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */} 
+                        {/* Todo  form goes here */} 
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
