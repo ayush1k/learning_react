5030e6544aabcab85f83ff4f6cf004e60964c216
Ayush Kumar <ayushkumar47834@gmail.com>
2025-07-07 19:19:55 +0000

    lec 21 done

--- 12-mega-blog/src/conf/conf.js
+++ 12-mega-blog/src/conf/conf.js
@@ -1,5 +1,5 @@
 const conf = {
-    appwriteURL: String(import.meta.env.VITE_APPWRITE_URl),
+    appwriteURL: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
