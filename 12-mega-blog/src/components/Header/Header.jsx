aaebc76790c8d48b7546a219888778b99dac0b14
Ayush Kumar <ayushkumar47834@gmail.com>
2025-07-10 13:36:39 +0000

    commit

--- 12-mega-blog/src/components/Header/Header.jsx
+++ 12-mega-blog/src/components/Header/Header.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import {Container, LogoutBtn, Logo} from '../index' 
+import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
 
 function Header() {
   return (
-    <div>
-      Header
-    </div>
+    const authStatus = useSelector((state) => state.auth.status)
+    const navigate = useNavigate()
+
+    const naItems = [
+      {
+      name: 'Home',
+      slug: "/",
+      active: true
+    }, 
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+  },
+  {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+  },
+  {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+  },
+  {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+  }
+    ]
+    <header></header>
   )
 }
 
