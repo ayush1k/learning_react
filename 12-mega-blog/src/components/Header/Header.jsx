badea863b686db36a842119d2d2733dce6f7c189
Ayush Kumar <ayushkumar47834@gmail.com>
2025-07-10 15:57:17 +0000

    lec 23 done

--- 12-mega-blog/src/components/Header/Header.jsx
+++ 12-mega-blog/src/components/Header/Header.jsx
@@ -1,42 +1,71 @@
 import React from 'react'
-import {Container, LogoutBtn, Logo} from '../index' 
-import {Link} from 'react-router-dom'
-import {useSelector} from 'react-redux'
-import {useNavigate} from 'react-router-dom'
+import { Container, LogoutBtn, Logo } from '../index'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 function Header() {
-  return (
-    const authStatus = useSelector((state) => state.auth.status)
-    const navigate = useNavigate()
+  const authStatus = useSelector((state) => state.auth.status)
+  const navigate = useNavigate()
 
-    const naItems = [
-      {
-      name: 'Home',
-      slug: "/",
-      active: true
-    }, 
+  const navItems = [
     {
-      name: "Login",
-      slug: "/login",
+      name: 'Home',
+      slug: '/',
+      active: true,
+    },
+    {
+      name: 'Login',
+      slug: '/login',
       active: !authStatus,
-  },
-  {
-      name: "Signup",
-      slug: "/signup",
+    },
+    {
+      name: 'Signup',
+      slug: '/signup',
       active: !authStatus,
-  },
-  {
-      name: "All Posts",
-      slug: "/all-posts",
+    },
+    {
+      name: 'All Posts',
+      slug: '/all-posts',
       active: authStatus,
-  },
-  {
-      name: "Add Post",
-      slug: "/add-post",
+    },
+    {
+      name: 'Add Post',
+      slug: '/add-post',
       active: authStatus,
-  }
-    ]
-    <header></header>
+    },
+  ]
+
+  return (
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px' />
+            </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
   )
 }
 
